bdf3f24547314565e7ffc0336ae6749a8e69b8dc
GenataSotirov <ewg_str@abv.bg>
2023-02-23 16:04:33 +0200

    Create Program.cs

---
+++ 06. Multiply Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Multiply_Table
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int a = num % 10;
+            num /= 10;
+            int b = num % 10;
+            int c = num / 10;
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    for (int l = 1; l <= c; l++)
+                    {
+                        Console.WriteLine($"{i} * {j} * {l} = {i*j*l};");
+                    }
+                }
+            }
+        }
+    }
+}
